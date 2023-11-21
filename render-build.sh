a5062e0fa8a03705fe8142dc3717a62b02541041
Marc raphael <marcraphael979@gmail.com>
2023-11-21 15:55:27 +0100

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,9 +1,6 @@
 # exit on error
-set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
 bundle exec rails db:migrate
 
 #if you have seeds to run add:
