95d7f8a0b7936fdbd9ad3ff5d24eef6007c65e8f
Marc raphael <marcraphael979@gmail.com>
2023-11-21 16:36:13 +0100

    Update user.rb
    - update relations
    - add validations
       - name
       - email
       - password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :group
-  has_many :money_tracks
+
+  has_many :groups, dependent: :destroy
+  has_many :money_tracks, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 50 }
+  validates :password, presence: true, length: { maximum: 30 }
 end
