0a8f6fdc85cf60d5d91dd577f7d8e2383e8b139a
Marc raphael <marcraphael979@gmail.com>
2023-11-21 16:34:03 +0100

    Add an abstract class

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
-  primary_abstract_class
+  # In Ruby on Rails, the `self.abstract_class = true` statement is used to indicate that the class is
+  # an abstract class. An abstract class is a class that cannot be instantiated directly, but can be
+  # inherited by other classes.
+  self.abstract_class = true
 end
