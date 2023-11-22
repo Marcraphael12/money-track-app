495930f89f93d1140cdbffd04fa0c06718057983
Marc raphael <marcraphael979@gmail.com>
2023-11-22 11:05:19 +0100

    Update for render

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module MoneyTrack
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+    config.assets.enabled = true
+
 
     # Configuration for the application, engines, and railties goes here.
     #
