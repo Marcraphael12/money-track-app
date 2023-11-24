9e7fa5368755904991f3bee23424bcddaf64b402
Marc raphael <marcraphael979@gmail.com>
2023-11-24 19:37:55 +0100

    Add models tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let(:user) { User.new(name: 'Polina', email: 'polina@mail.com', password: '123456') }
+
+  before { user.save }
+
+  describe 'assocciations' do
+    it { should have_many(:money_tracks) }
+    it { should have_many(:groups) }
+  end
+
+  describe 'validations' do
+    context 'with correct parameters' do
+      it 'name, email, password should be present' do
+        expect(user).to be_valid
+      end
+    end
+
+    context 'with wrong parameters' do
+      it 'name should be present' do
+        user.name = nil
+        expect(user).to_not be_valid
+      end
+
+      it 'email should be present' do
+        user.email = nil
+        expect(user).to_not be_valid
+      end
+
+      it 'email should be present' do
+        user.password = nil
+        expect(user).to_not be_valid
+      end
+
+      it 'name should not be too long' do
+        user.name = 'Polina' * 10
+        expect(user).to_not be_valid
+      end
+
+      it 'email should not be too long' do
+        user.email = 'polina.stamenova@mail.com' * 20
+        expect(user).to_not be_valid
+      end
+
+      it 'password should not be too long' do
+        user.password = 'mysicretpassword' * 10
+        expect(user).to_not be_valid
+      end
+    end
+  end
+end
