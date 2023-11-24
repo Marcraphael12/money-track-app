3ff2f7e0d0784de79fe463f18ef376202508a54a
Marc raphael <marcraphael979@gmail.com>
2023-11-24 23:37:35 +0100

    update for render

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,4 @@
-# frozen_string_literal: true
-
-require 'active_support/core_ext/integer/time'
+require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -31,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,6 @@
 # exit on error
 
 bundle install
-bundle exec rails db:drop
-bundle exec rails db:create
 bundle exec rails db:migrate
 
 #if you have seeds to run add:
